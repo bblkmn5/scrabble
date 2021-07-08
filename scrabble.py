e1cfb878039ecb0bfc66ffc577309426c09bdfde
Ben Blackman <bblkmn5@gmail.com>
2021-07-08 14:16:55 -0700

    build dictionary and add missing tile

--- scrabble.py
+++ scrabble.py
@@ -6,3 +6,10 @@ letters = [
     ]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+#combine two lists into dictionary
+letter_to_points = {letter:point for letter, point in zip(letters, points)}
+# print(letter_to_points)
+
+#add blank tile to dictionary
+letter_to_points[" "] = 0
+# print(letter_to_points)
